888a8e6908dd3a40c3055fe00d7b82c880a8b70f
Ming Chang <mail@mingchang.tw>
2022-04-18 18:56:32 +0800

    frontend: replace frontend with template.
    from https://www.creative-tim.com/product/now-ui-kit-angular

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -1,37 +1,43 @@
-import { Component } from '@angular/core';
-import {MenuItem} from 'primeng/api';
-import {HttpClient} from "@angular/common/http";
-import {ArticleList} from "../shared/model/ArticleList";
+import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/filter';
+import { DOCUMENT } from '@angular/common';
+import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
+export class AppComponent implements OnInit {
+    private _router: Subscription;
+    @ViewChild(NavbarComponent) navbar: NavbarComponent;
 
-export class AppComponent {
+    constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+    ngOnInit() {
+        var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+            if (window.outerWidth > 991) {
+                window.document.children[0].scrollTop = 0;
+            }else{
+                window.document.activeElement.scrollTop = 0;
+            }
+            this.navbar.sidebarClose();
 
-  items: MenuItem[] = [
-    {
-      label: 'Blog'
-    },
-    {
-      label: 'About'
+            this.renderer.listen('window', 'scroll', (event) => {
+                const number = window.scrollY;
+                var _location = this.location.path();
+                _location = _location.split('/')[2];
+
+                if (number > 150 || window.pageYOffset > 150) {
+                    navbar.classList.remove('navbar-transparent');
+                } else if (_location !== 'login' && this.location.path() !== '/nucleoicons') {
+                    // remove logic
+                    navbar.classList.add('navbar-transparent');
+                }
+            });
+        });
     }
-  ];
-
-  articleList: ArticleList[] | undefined
-
-  constructor(
-    private http: HttpClient
-  ) {
-  }
-
-  ngOnInit() {
-    this.http.get('http://localhost:4200/service/list').pipe().subscribe(data => {
-      this.articleList = data as ArticleList[]
-      console.log(this.articleList)
-    })
-  }
-
 }
--- blog/src/shared/model/ArticleList.ts
+++
@@ -1,6 +0,0 @@
-export class ArticleList {
-  id: string | undefined;
-  title: string | undefined;
-  content: string | undefined;
-  updateDate: Date | undefined;
-}
